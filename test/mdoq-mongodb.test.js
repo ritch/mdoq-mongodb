534bb39eb7acf6e6fca610a590d5fbacc44efb29
Ritchie <skawful@gmail.com>
2012-02-29 10:07:22 -0700

    various fixes and tests

--- test/mdoq-mongodb.test.js
+++ test/mdoq-mongodb.test.js
@@ -5,83 +5,50 @@ var mdoq = require('mdoq')
   , users = mdoq.use(require('../')).use('test-db').use('/users')
 ;
 
-
-describe('mongodb', function(){
+describe('MongoDB Middleware', function(){
   
-  it('should use the test-db and users collection', function(done){
-
-    users.first(function(err, res) {
-      expect(this.db).to.equal('test-db');
-      expect(this.collection).to.equal('users');
-    })
-
-    done();
+  it('should remove all users', function(done){
+    users.del(done);
   })
   
-  it('should create a user', function(done){
+  it('should insert a new user', function(done){
     users.post(user, function(err, res) {
-      done(err);
-    });
+      
+      console.info(res);
+      expect(res.email).to.equal(user.email);
+      expect(res._id).to.be.a('string');
+      done();
+    })
   })
   
-  it('should get the user', function(done){
-    users.first(user, function(err, res) {
-      expect(res).to.be.a('object');
-      expect(res.email).to.equal(user.email);
-      
-      done(err);
-    });
-  })
-    
-  it('should iterate over each result', function(done){
-    users.limit(3).each(function(err, doc, i) {
-      if(i === 2) done(err);
+  it('should return a user by query', function(done){
+    users.get(user, function(err, res) {
+      expect(res).to.eql(user);
+      done()
     })
   })
 
-  it('should get the first 3 users', function(done){
-    users.first(3, function(err, res) {
-      expect(res.length).to.equal(3);
-      done(err);
-    })
-  })
-  
-  it('should get all users', function(done){
-    users.all(function(err, res) {
-      expect(res.length > 3).to.be.ok;
-      done(err);
+  it('should update a user by id', function(done){
+    var updates = {updated: true};
+    
+    users.get({email: user.email}).put(updates, function(err, res) {      
+      expect(res).to.eql(updates);
+      done();
     });
   })
   
-  it('should get the second page of three users', function(done){
-    users.page(2, 3).get(function(err, res) {
-      expect(res.length).to.equal(3);
-      done(err);
-    });
-  })
-  
-  it('should return a count of all users', function(done){
-    users.get(user).count(function(err, res, count) {
-      expect(count).to.be.a('number');
-      done(err);
-    })
-  })
-  
-  it('should get a user and modify it', function(done){
-    users.get({_id: user._id}).put({updated: true}, function(err) {
-      done(err);
-    });
-  })
-  
-  it('should get one user', function(done){
-      users.first({_id: user._id}, function(err, res) {
-        expect(res.updated).to.equal(true);
-        done(err);
+  it('should error when provided improper input', function(done){
+    users
+      .use(function(req, res, next) {
+        this.req.query = 'a bad query';
+        next();
       })
+      .use('/users')
+      .get(function(err, res) {
+        expect(err).to.be.a('object');
+        done()
+      })
+    ;
   })
-  
-  it('should delete a user by query', function(done){
-    users.del(user, done);
-  })
-  
+
 })
